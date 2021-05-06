b20b715de55e7fd689e5f03b07a119802df795d5
fallingsappy <novikovvlal@gmail.com>
2021-05-06 23:45:17 +0300

    added feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          FeatureB
         </p>
         <a
           className="App-link"
